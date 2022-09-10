917636f5403eac9deea361db0c525b91a62706c2
Gabriel Rissi <gbrissi123@gmail.com>
2022-09-09 23:29:36 -0300

    creating migrations

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { Options } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import path from 'path';
 
 const config: Options = {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        glob: '!(*.d).{js,ts}'
+    },
     entities: [Post],
     dbName: 'lireddit',
     type: 'postgresql',
