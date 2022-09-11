294ea69612ca56ce6e207765280e27c6cc497670
Gabriel Rissi <gbrissi123@gmail.com>
2022-09-11 11:15:58 -0300

    adding graphql to server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import {MikroORM} from "@mikro-orm/core"
 import { __prod__ } from "./constants";
-// import { Post } from "./entities/Post";
+import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config" 
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async() => {
     const orm = await MikroORM.init(microConfig);
@@ -10,9 +13,15 @@ const main = async() => {
     
     const app = express();
 
-    app.get('/', (_, res) => {
-        res.send('Hello!')
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        })
     })
+
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app})
 
     app.listen(4000, () => {
         console.log("\nServer started at port 4000!")
@@ -22,5 +31,3 @@ const main = async() => {
 main().catch((err) => {
     console.error(err)
 });
-
-console.log("Hello there!")
