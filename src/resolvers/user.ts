9929c93caf410cf122f9f8231cf942dd6baff799
Gabriel Rissi <gbrissi123@gmail.com>
2022-09-17 11:39:00 -0300

    feat: creating session cookies to user auth

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "../types";
-import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType } from "type-graphql";
+import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType, Query } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from 'argon2'
 
@@ -30,6 +30,18 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() {req, em}: MyContext
+    ) {
+        // You are not logged in!
+        if(!req.session.userId) {
+            return null
+        }
+        const user = await em.findOne(User, {id: req.session.userId})
+        return user
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
