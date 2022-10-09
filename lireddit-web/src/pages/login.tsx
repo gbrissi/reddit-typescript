4df77066f02457e4ed2096d89928bff44ddbdf3d
gbrissi <gbrissi123@gmail.com>
2022-10-09 20:10:22 -0300

    refactoring and setting csr for register and login

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -1,12 +1,13 @@
-import React, { useMemo } from 'react'
-import {Form, Formik} from 'formik'
-import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react';
-import Wrapper from "../components/Wrapper"
-import { InputField } from '../components/InputField';
-import { useMutation } from 'urql';
-import { useLoginMutation, useRegisterMutation } from '../generated/graphql';
-import { toErrorMap } from '../utils/toErrorMap';
+import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
+import React from 'react';
+import { InputField } from '../components/InputField';
+import Wrapper from "../components/Wrapper";
+import { useLoginMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { toErrorMap } from '../utils/toErrorMap';
 
 const Login: React.FC<{}> = ({}) => {
     const router = useRouter();
@@ -36,4 +37,4 @@ const Login: React.FC<{}> = ({}) => {
     );
 }
 
-export default Login;
+export default withUrqlClient(createUrqlClient)(Login);
