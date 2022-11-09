bd72debf40671ac20932196529e86e1532753021
gbrissi <gbrissi123@gmail.com>
2022-11-08 22:39:22 -0300

    implementing load more button

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Text, Box, Heading, Link, Stack } from "@chakra-ui/react"
+import { Text, Box, Heading, Link, Stack, Flex, Button } from "@chakra-ui/react"
 import { withUrqlClient } from "next-urql"
 import { Layout } from "../components/Layout"
 import { usePostsQuery } from "../generated/graphql"
@@ -6,22 +6,30 @@ import { createUrqlClient } from "../utils/createUrqlClient"
 import NextLink from 'next/link'
 
 const Index = () => { 
-  const [{data}] = usePostsQuery({
+  const [{data, fetching}] = usePostsQuery({
     variables: {
       limit: 10
     }
   });
+
+  if(!fetching && !data) {
+    return <div>your query failed</div>
+  }
+
   return (
     <Layout>
-      <NextLink href='/create-post'>
-          <Link>createPost</Link>
-      </NextLink>
+      <Flex align='center'>
+        <Heading>LiReddit</Heading>
+        <NextLink href='/create-post'>
+            <Link ml='auto'>createPost</Link>
+        </NextLink>
+      </Flex>
       <br/>
-      {!data ? (
-      <div>loading...</div>
+      {fetching && !data ? (
+        <div>loading...</div>
       ) : (
         <Stack spacing = {8}>
-          {data.posts.map((p) => 
+          {data!.posts.map((p) => 
             <Box key={p.id} p={5} shadow="md" borderWidth={1}>
               <Heading fontSize='xl'>{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -29,6 +37,9 @@ const Index = () => {
           )}
         </Stack>
       )}
+      {data ? (<Flex>
+        <Button isLoading={fetching} m='auto' my={8}>load more</Button>
+      </Flex>) : null}
     </Layout>
   )
 }
