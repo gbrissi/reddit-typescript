533c86ad52bebd39d1d41ab82324919b76d17d46
gbrissi <gbrissi123@gmail.com>
2022-11-14 23:56:45 -0300

    creating new creator relational field for post

--- lireddit-server/src/resolvers/post.ts
+++ lireddit-server/src/resolvers/post.ts
@@ -38,17 +38,42 @@ export class PostResolver {
         const realLimit = Math.min(50, limit);
         const realLimitPlusOne = realLimit + 1;
 
-        const qb = conn
-            .getRepository(Post)
-            .createQueryBuilder("post")
-            .orderBy('"createdAt"', "DESC")
-            .take(realLimitPlusOne)
-
+        const replacements: any[] = [realLimitPlusOne]
         if(cursor) {
-            qb.where('"createdAt" < :cursor', {cursor: new Date(parseInt(cursor))})
+            replacements.push(new Date(parseInt(cursor)));
         }
 
-        const posts = await qb.getMany()
+        const posts = await conn.query(`
+            select p.*,
+            u.username 
+            json_build_object(
+                'id', u.id,
+                'username', u.username,
+                'email', u.email,
+                ) creator
+            from post p
+            inner join public.user u on u.id = p."creatorId"
+            ${cursor ? `where p."createdAt" < $2` : ''}
+            order by p."createdAt" DESC
+            limit $1
+        `, replacements)
+
+        //  = conn
+        //     .getRepository(Post)
+        //     .createQueryBuilder("post")
+        //     .innerJoinAndSelect(
+        //         "p.creator",
+        //         "u",
+        //         'u.id =  p."creatorId"',
+        //     )
+        //     .orderBy('p."createdAt"', "DESC")
+        //     .take(realLimitPlusOne)
+
+        // if(cursor) {
+        //     qb.where('p."createdAt" < :cursor', {cursor: new Date(parseInt(cursor))})
+        // }
+
+        // const posts = await qb.getMany()
 
         return {posts: posts.slice(0, realLimit), hasMore: posts.length === realLimitPlusOne};
     }
